21e4f81088623de3879fb11d7aa669b806925243
Mike Jandreau <mikejandreau@gmail.com>
2017-08-07 15:53:03 -0400

    initial commit

---
+++ header.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Base_Install
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class('sideNavBody'); ?>>
+
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'baseinstall' ); ?></a>
+
+	<header id="masthead" class="site-header">
+		<div class="site-navbar">
+			<div class="site-branding">
+				<?php
+				the_custom_logo();
+				if ( is_front_page() && is_home() ) : ?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<?php else : ?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+				endif;
+
+				$description = get_bloginfo( 'description', 'display' );
+				if ( $description || is_customize_preview() ) : ?>
+				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+				<?php
+				endif; ?>
+			</div><!-- .site-branding -->
+			
+			<button class="menu-toggle" tabindex="0" aria-label="Menu" aria-controls="primary-menu"><?php esc_html_e( 'Menu', 'baseinstall' ); ?><span>toggle menu</span></button>
+
+			<nav id="site-navigation" class="main-navigation">
+				<h3 class="screen-reader-text">Main Navigation</h3>
+				<?php 
+				wp_nav_menu( array(
+					'theme_location'	=> 'menu-1',
+					'echo'				=> true,
+					'depth'				=> 10, 
+					'container'			=> 'ul',
+					'menu_class'		=> 'nav-menu', 
+					'menu_id'			=> 'primary-menu',
+					'items_wrap'		=> '<ul id="%1$s" class="%2$s">%3$s</ul>',
+					'walker'			=> new baseinstall_walker_nav_menu
+					) ); 
+					?>
+				</nav><!-- #site-navigation -->
+		</div>
+	</header><!-- #masthead -->
+
+	<div id="content-wrap" class="site-content-wrap">
+	<div id="content" class="site-content">
---
+++ template-parts/strapline.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The strapline section for our theme
+ *
+ * This is the template that displays .strapline (optional)
+ * the strapline section of the site displays below the hero and is often used for a "triple feature" sort of thing
+ * if you don't want/need it, comment out or delete get_template_part('strapline'); in base.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Base_Install
+ */
+?>
+
+<div class="section strapline">
+	<div class="container">
+		<div class="row">
+			<div class="column sm-4">
+				<div class="subsection">
+					<p>Put some</p>
+				</div>
+			</div>
+			<div class="column sm-4">
+				<div class="subsection">
+					<p>content</p>
+				</div>
+			</div>
+			<div class="column sm-4">
+				<div class="subsection">
+					<p>in here.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
